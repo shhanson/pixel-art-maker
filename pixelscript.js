fdf6a5e6aeb87deee96f38b464fa9aef46bc65e9
Sarah Hanson <sarahh.hanson@gmail.com>
2017-04-29 12:40:08 -0500

    Optimized canvas divs creation at the expense of the x/y axes highlight

--- pixelscript.js
+++ pixelscript.js
@@ -1,9 +1,10 @@
 //CONSTANT VALUES
+const COLORS = ["#000000", "#0000BC", "#0000FC", "#004058", "#005800", "#0058F8", "#006800", "#007800", "#0078F8", "#008888", "#00A800", "#00A844", "#00B800", "#00E8D8", "#00FCFC", "#3CBCFC", "#4428BC", "#503000", "#58D854", "#58F898", "#6844FC", "#6888FC", "#787878", "#7C7C7C", "#881400", "#940084", "#9878F8", "#A4E4FC", "#A80020", "#A81000", "#AC7C00", "#B8B8F8", "#B8F818", "#B8F8B8", "#B8F8D8", "#BCBCBC", "#D800CC", "#D8B8F8", "#D8F878", "#E40058", "#E45C10", "#F0D0B0", "#F83800", "#F85898", "#F87858", "#F878F8", "#F8A4C0", "#F8B800", "#F8B8F8", "#F8D878", "#F8D8F8", "#F8F8F8", "#FCA044", "#FCE0A8", "#FCFCFC", "#FFFFFF"];
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
-const COLORS = ["#000000", "#0000BC", "#0000FC", "#004058", "#005800", "#0058F8", "#006800", "#007800", "#0078F8", "#008888", "#00A800", "#00A844", "#00B800", "#00E8D8", "#00FCFC", "#3CBCFC", "#4428BC", "#503000", "#58D854", "#58F898", "#6844FC", "#6888FC", "#787878", "#7C7C7C", "#881400", "#940084", "#9878F8", "#A4E4FC", "#A80020", "#A81000", "#AC7C00", "#B8B8F8", "#B8F818", "#B8F8B8", "#B8F8D8", "#BCBCBC", "#D800CC", "#D8B8F8", "#D8F878", "#E40058", "#E45C10", "#F0D0B0", "#F83800", "#F85898", "#F87858", "#F878F8", "#F8A4C0", "#F8B800", "#F8B8F8", "#F8D878", "#F8D8F8", "#F8F8F8", "#FCA044", "#FCE0A8", "#FCFCFC", "#FFFFFF"];
 const DIV_LENGTH = 20;
 const NUM_COLORS = COLORS.length;
+const NUM_DIVS = (CANVAS_WIDTH / DIV_LENGTH) * (CANVAS_HEIGHT / DIV_LENGTH);
 
 //Global variables :(
 var currentColor = "black";
@@ -12,12 +13,6 @@ var mouseoverFlag = false;
 
 //Setup canvas (drawing area)
 function setupCanvas() {
-
-    //Add listener to the document to keep track of whether or not mouseup occurs anywhere on screen (fixes issue of mousedown over canvas -> mouseup outside of canvas)
-    document.addEventListener("mouseup", function() {
-        mousedownFlag = false;
-    });
-
     //Grab canvasarea div
     let canvas = document.querySelector(".canvasarea");
 
@@ -25,54 +20,45 @@ function setupCanvas() {
     canvas.style.height = CANVAS_HEIGHT + "px";
     canvas.style.width = CANVAS_WIDTH + "px";
 
+    //Add listener to the document to keep track of whether or not mouseup occurs anywhere on screen (fixes issue of mousedown over canvas -> mouseup outside of canvas)
+    document.addEventListener("mouseup", function() {
+        mousedownFlag = false;
+    });
+
     //Loop to generate each cell div within the canvas
-    for (let i = 0; i < CANVAS_WIDTH / DIV_LENGTH; i++) {
-        for (let j = 0; j < CANVAS_HEIGHT / DIV_LENGTH; j++) {
-            //Create each cell div
-            let cell = document.createElement("div");
-            cell.className = "canvasdiv";
+    for (let i = 0; i < NUM_DIVS; i++) {
 
-            //Prevents browser "thinking" that the div is meant to be dragged
-            cell.setAttribute("onmousedown", "return false");
+        //Create each cell div
+        let cell = document.createElement("div");
+        cell.className = "canvasdiv";
 
-            //Set div width and height based on DIV_LENGTH const
-            cell.style.width = DIV_LENGTH + "px";
-            cell.style.height = DIV_LENGTH + "px";
+        //Prevents browser "thinking" that the div is meant to be dragged
+        cell.setAttribute("onmousedown", "return false");
 
-            //Distinguishes the x-axis
-            if (i === (CANVAS_WIDTH / DIV_LENGTH) / 2) {
-                cell.style.borderTop = "2px solid darkgray";
-            }
+        //Set div width and height based on DIV_LENGTH const
+        cell.style.width = DIV_LENGTH + "px";
+        cell.style.height = DIV_LENGTH + "px";
 
-            //Distinguishes the y-axis
-            if (j === (CANVAS_HEIGHT / DIV_LENGTH) / 2) {
-                cell.style.borderLeft = "2px solid darkgray";
-            }
-
-            //Event listener for clicking the cell
-            cell.addEventListener("click", function() {
+        //Event listener for clicking the cell
+        cell.addEventListener("click", function() {
+            cell.style.backgroundColor = currentColor;
+            cell.style.borderColor = currentColor;
+        });
+        //Event listener for holding the mouse down, aids with click and drag functionality
+        cell.addEventListener("mousedown", function() {
+            cell.style.backgroundColor = currentColor;
+            cell.style.borderColor = currentColor;
+            mousedownFlag = true;
+        });
+        //Event listener for mousing over the div, aids with click and drag functionality
+        cell.addEventListener("mouseover", function() {
+            if (mousedownFlag) {
                 cell.style.backgroundColor = currentColor;
                 cell.style.borderColor = currentColor;
-            });
-
-            //Event listener for holding the mouse down, aids with click and drag functionality
-            cell.addEventListener("mousedown", function() {
-                cell.style.backgroundColor = currentColor;
-                cell.style.borderColor = currentColor;
-                mousedownFlag = true;
-            });
-
-            //Event listener for mousing over the div, aids with click and drag functionality
-            cell.addEventListener("mouseover", function() {
-                if (mousedownFlag) {
-                    cell.style.backgroundColor = currentColor;
-                    cell.style.borderColor = currentColor;
-                }
-            });
-
-            //Appends each div to the canvasarea
-            canvas.appendChild(cell);
-        }
+            }
+        });
+        //Appends each div to the canvasarea
+        canvas.appendChild(cell);
     }
 }
 
@@ -131,7 +117,6 @@ function setupControlPanel() {
                 canvasDivs[i].style.borderColor = "lightgray"
             }
         }
-
     });
 }
 
